60f80ef09406903df9447218623e79749c3e8520
Darlan Selzlein <sdarlanset@gmail.com>
2020-06-16 10:29:38 -0300

    cart complete

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,17 @@
 class StoreController < ApplicationController
-
+  before_action :count, only: [:index]
+  
   def index
+    @accesses = session[:count]
     @products = Product.order(:title)
   end
+
+  private
+    def count
+      if session[:count].nil?
+        session[:count] = 1
+      else
+        session[:count] += 1
+      end
+    end
 end
